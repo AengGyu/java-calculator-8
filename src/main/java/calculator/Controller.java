b625ad108b555316c732460362f50156a4e646d1
AengGyu <cyg0115@naver.com>
2025-10-15 20:18:31 +0900

    feat : Controller 작성

---
+++ src/main/java/calculator/Controller.java
@@ -0,0 +1,39 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+import java.util.Set;
+
+public class Controller {
+
+    public static void run() {
+
+        String input = Console.readLine();
+
+        Set<Character> separators = getSeparators(input);
+        List<Integer> operands = getOperands(input, separators);
+
+        int sum = Calculator.sum(operands);
+
+        System.out.println("결과 : " + sum);
+    }
+
+    private static Set<Character> getSeparators(String input) {
+        Set<Character> separators = Parser.parseSeparator(input);
+
+        if (separators == null) {
+            return Set.of(',', ':');
+        }
+
+        else {
+            separators.add(',');
+            separators.add(':');
+            return separators;
+        }
+    }
+
+    private static List<Integer> getOperands(String input, Set<Character> separators) {
+        return Parser.parseOperands(input, separators);
+    }
+}
