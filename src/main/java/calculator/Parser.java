1ec6069119f9455a9a78b79eb738020404ddc9a2
AengGyu <cyg0115@naver.com>
2025-10-20 11:05:08 +0900

    refactor : 메서드 분리

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -6,14 +6,12 @@ import java.util.List;
 import java.util.Set;
 
 /**
- * parseSeparator : 기본 구분자를 제외한 커스텀 구분자를 파싱해서 리스트로 반환
- * parseOperands : 피연산자를 파싱해서 리스트로 반환
+ * parseSeparator : 기본 구분자를 제외한 커스텀 구분자를 파싱해서 리스트로 반환 parseOperands : 피연산자를 파싱해서 리스트로 반환
  */
 public class Parser {
 
     /**
-     * 문자열을 입력 받아서 커스텀 구분자가 있는 경우 커스텀 구분자와 기본 구분자를 포함하여 Set에 저장 후 반환
-     * 커스텀 구분자가 없는 경우 기본 구분자만 포함한 Set 반환
+     * 문자열을 입력 받아서 커스텀 구분자가 있는 경우 커스텀 구분자와 기본 구분자를 포함하여 Set에 저장 후 반환 커스텀 구분자가 없는 경우 기본 구분자만 포함한 Set 반환
      */
     public static Set<Character> parseSeparator(String input) {
 
@@ -55,10 +53,9 @@ public class Parser {
     }
 
     /**
-     * // 와 \n 사이의 입력된 커스텀 구분자 문자열을 반환하는 메서드
-     * 메서드 이름 뭘로 하지 ??????
+     * // 와 \n 사이의 입력된 커스텀 구분자 문자열을 반환하는 메서드 메서드 이름 뭘로 하지 ??????
      */
-    private static String getString(String input) {
+    private static String getBeforeParsing(String input) {
         int idx = getIdxOfCustomSeparatorEnd(input);
         checkCustomSeparator(idx);
 
@@ -66,18 +63,21 @@ public class Parser {
     }
 
     /**
-     * 커스텀 구분자를 Set에 저장 후 반환하는 메서드
-     * 숫자는 커스텀 구분자에 포함될 수 없음
+     * 커스텀 구분자를 Set에 저장 후 반환하는 메서드 숫자는 커스텀 구분자에 포함될 수 없음
      */
     private static Set<Character> getCustomSeparators(String input) {
         Set<Character> separators = new HashSet<>();
 
-        String customSeparators = getString(input);
+        String customSeparators = getBeforeParsing(input);
 
         for (int i = 0; i < customSeparators.length(); i++) {
             if (isNumber(customSeparators.charAt(i))) {
                 throw new IllegalArgumentException("숫자는 구분자로 사용할 수 없습니다.");
             }
+            if (customSeparators.charAt(i) == '-') {
+                throw new IllegalArgumentException("'-'는 구분자로 사용할 수 없습니다.");
+            }
+
             separators.add(customSeparators.charAt(i));
         }
 
@@ -87,12 +87,8 @@ public class Parser {
         return separators;
     }
 
-    // TODO: 구분자가 연속해서 오는 경우 예외처리
-    // TODO: '-' 는 어떻게 처리할 것인지
-
     /**
-     * 커스텀 구분자가 있으면 떼어낸 후 피연산자를 하나씩 리스트에 저장 후 리턴
-     * 구분자에 포함되지 않는 문자가 피연산자 사이에 있는 경우 예외처리
+     * 커스텀 구분자가 있으면 떼어낸 후 피연산자를 하나씩 리스트에 저장 후 리턴 구분자에 포함되지 않는 문자가 피연산자 사이에 있는 경우 예외처리
      */
     private static List<Integer> getOperands(Set<Character> separators, String expression) {
 
@@ -102,16 +98,16 @@ public class Parser {
         for (int i = 0; i < expression.length(); i++) {
             char operand = expression.charAt(i);
 
-            if (isNumber(operand)) {
-                num = num * 10 + operand - '0';
-            } else {
+            if (!isNumber(operand)) {
                 if (!separators.contains(operand)) {
-                    checkExpression(operand);
+                    checkOperator(operand);
                 }
 
                 operands.add(num);
                 num = 0;
             }
+
+            num = num * 10 + operand - '0';
         }
 
         operands.add(num);
@@ -120,9 +116,7 @@ public class Parser {
     }
 
     /**
-     * 계산식을 반환하는 메서드
-     * 커스텀 구분자가 없다면 input을 그대로 반환
-     * 커스텀 구분자가 있으면 커스텀 구분자가 입력된 부분을 제외한 계산식만 반환
+     * 계산식을 반환하는 메서드 커스텀 구분자가 없다면 input을 그대로 반환 커스텀 구분자가 있으면 커스텀 구분자가 입력된 부분을 제외한 계산식만 반환
      */
     private static String getExpression(String input, Set<Character> separators) {
 
@@ -136,7 +130,7 @@ public class Parser {
     /**
      * 계산식에 음수(-) 또는 구분자로 지정되지 않은 문자가 들어있는 경우 예외처리
      */
-    private static void checkExpression(char operand) {
+    private static void checkOperator(char operand) {
         if (operand == '-') {
             throw new IllegalArgumentException("음수는 피연산자로 사용할 수 없습니다.");
         }
